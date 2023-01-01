f22f2791b2f0b68ec0436c5d38a8993975ae0fb0
tanaliga100 <jordantanaliga@gmail.com>
2023-01-01 16:58:58 +0800

    connect-to-ethereal

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,4 +1,22 @@
+const nodemailer = require("nodemailer");
+
 const sendEmailController = async (req, res) => {
-  res.send("Email sent");
+  let testAccount = await nodemailer.createTestAccount();
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "aylin36@ethereal.email",
+      pass: "aDz9kr5jvxsaa4JN9H",
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"jordantanaliga100" <jordantanaliga@gmail.com>',
+    to: "jordantanaliga@gmail.com",
+    subject: "Hello",
+    html: "<b>Hello world, sending email with nodeJS</b>",
+  });
+  res.json({ info });
 };
 module.exports = sendEmailController;
