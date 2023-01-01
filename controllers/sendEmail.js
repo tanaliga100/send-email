c510bc83b01e77b6c5e93be10037348756b089cb
tanaliga100 <jordantanaliga@gmail.com>
2023-01-01 21:29:14 +0800

    config-sendGrid

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,4 +1,5 @@
-// const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 
 // const sendEmailControllerTesting = async (req, res) => {
 //   let testAccount = await nodemailer.createTestAccount();
@@ -20,25 +21,10 @@
 //   res.json({ info });
 // };
 
-// sending using SENDGRID
-// const sendEmailController = async (req, res) => {
-//   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//   const msg = {
-//     to: "jordantanaliga20@gmail.com", // Change to your recipient
-//     from: "jordantanaliga@gmail.com", // Change to your verified sender
-//     subject: "Sending with SendGrid is Fun",
-//     text: "and easy to do anywhere, even with Node.js",
-//     html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-//   };
-//   const response = await sgMail.send(msg);
-//   res.json(response);
-// };
-
-const sgMail = require("@sendgrid/mail");
 const sendEmail = async (req, res) => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: "jordantanaliga20@gmail.com", // Change to your recipient
+    to: "jordantanaliga@gmail.com", // Change to your recipient
     from: "jordantanaliga@gmail.com", // Change to your verified sender
     subject: "Sending with SendGrid is Fun",
     text: "and easy to do anywhere, even with Node.js",
